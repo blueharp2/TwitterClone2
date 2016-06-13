4208956a32a92bbcb3e8664c577a90df392d5454
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2016-06-13 14:20:42 -0700

    JSON Parser works

--- TwitterClone2/ViewController.swift
+++ TwitterClone2/ViewController.swift
@@ -12,14 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        JSONParser.tweetJSONFrom(JSONParser.JSONData()) { (success, tweets) in
+            if success{
+                for tweet in tweets!{
+                    print(tweet.text)
+                }
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
