1ec22fd658b694a29112ecef8a9762bfad27d278
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2016-06-15 16:55:22 -0700

    Continued work on Profile VC

--- TwitterClone2/ProfileViewController.swift
+++ TwitterClone2/ProfileViewController.swift
@@ -9,24 +9,36 @@
 import UIKit
 
 class ProfileViewController: UIViewController, Identity {
+    
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
-
+    
+    var user = User?()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
+        self.update()
         
-      
-        
-        
+//        if let user = self.user{
+//            self.userNameLabel.text = user.name
+//            self.locationLabel.text = user.location
+//        }
     }
+    
+    func update(){
+        API.shared.GETOAuthUser { (user) in
+            if let user = user{
+                self.user = user
+            }
+        }
+    }
+    
+    
 
     @IBAction func closeButton(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
