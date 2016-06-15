7ab76cda46c492c815810afdd7ee9d9031ea9ba7
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2016-06-15 16:21:57 -0700

    Added Profile View Controller

---
+++ TwitterClone2/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  TwitterClone2
+//
+//  Created by Lindsey on 6/15/16.
+//  Copyright © 2016 Lindsey Boggio. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController, Identity {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+}
