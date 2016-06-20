b02fcbe992f8fcf77b2a5a40c4e48d90ce504dfe
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2016-06-20 16:27:59 -0700

    Start of Xib

---
+++ TweetCell.swift
@@ -0,0 +1,30 @@
+//
+//  TweetCell.swift
+//  TwitterClone2
+//
+//  Created by Lindsey on 6/20/16.
+//  Copyright © 2016 Lindsey Boggio. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
