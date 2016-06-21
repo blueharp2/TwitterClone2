c81fc50476ac6255163ce40d393ed8ca24d172ae
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2016-06-20 17:32:53 -0700

    Worked on Xib

--- TweetCell.swift
+++ TweetCell.swift
@@ -14,17 +14,47 @@ class TweetCell: UITableViewCell {
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetLabel: UILabel!
     
+    var cache: Cache<UIImage>?{
+        if let delegate = UIApplication.sharedApplication().delegate as? AppDelegate{
+            return delegate.cache
+        }
+        return nil
+    }
     
+    var tweet: Tweet!{
+        didSet{
+            self.tweetLabel.text = tweet.text
+            
+            if let user = self.tweet.user{
+                self.userNameLabel.text = user.screenName
+            
+            
+            if let image = cache?.read(user.profileImageURL){
+                self.profileImageView.image = image
+                return
+            }
+            
+            API.shared.getImages(user.profileImageURL, completion: { (image) in
+                self.cache?.write(image, key: user.profileImageURL)
+                self.profileImageView.image = image
+            })
+        }
+    }
+}
+    func setupTweetCell(){
+        self.profileImageView.clipsToBounds = true
+        self.profileImageView.layer.cornerRadius = 15.0
+        self.preservesSuperviewLayoutMargins = false
+        self.separatorInset = UIEdgeInsetsMake(0.0, 10.0, 0.0, 10.0)
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setupTweetCell()
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
